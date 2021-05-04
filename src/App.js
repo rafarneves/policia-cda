50f37492f61d5ef64d31555939c76b4c0127000e
Rafael Neves <rafael.neves@info.ufrn.br>
2021-05-04 00:32:54 -0300

    Remove lixo e adiciona funcionalidades

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const colunas = [
+  {
+    dataField: 'id',
+    text: 'ID'
+  },
+  {
+    dataField: 'nome',
+    text: 'Nome'
+  },
+  {
+    dataField: 'senha',
+    text: 'Senha'
+  }
+];
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: [],
+      coluns: colunas
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://my-json-server.typicode.com/cidadealta/exercise/usuarios')
+      .then(({ data }) => {
+        this.setState({ data })
+      })
+  }
+
+  render() {
+    const { data, coluns } = this.state
+    return (
+      <div>
+        <Table bordered responsive>
+          <thead>
+            <tr>
+              {coluns.map(key =>
+                <th style={{textAlign: 'left'}}>{key.text}</th>
+              )}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => 
+              <tr>
+                <td key={index}>{item.id}</td>
+                <td key={index}>{item.nome}</td>
+                <td key={index}>{item.senha}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
